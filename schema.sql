67cfade14b00d8ddff5899ea52659cea72e675ee
Fred Schuck <fredschuck@gmail.com>
2023-08-19 07:58:02 -0400

    Started database schema for routine, exercise, and users.

--- schema.sql
+++ schema.sql
@@ -7,3 +7,26 @@ DROP TABLE IF EXISTS "user" CASCADE;
 
 -- CREATE TABLES
 
+CREATE TABLE routine (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    user_id INTEGER REFERENCES "user"(id)
+);
+
+CREATE TABLE exercise (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    sets INTEGER,
+    reps INTEGER,
+    weight INTEGER,
+    time INTEGER,
+    speed INTEGER,
+    distance INTEGER,
+    routine_id INTEGER REFERENCES routine(id)
+);
+
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
