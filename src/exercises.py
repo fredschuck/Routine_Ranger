65c73a77350a94a7fe58d6f04f05e8b69edd9468
FredSchuck <fredschuck@gmail.com>
2023-08-19 12:59:49 -0400

    Initial commit

---
+++ src/exercises.py
@@ -0,0 +1,31 @@
+from src.models import db, Exercise, RoutineExercise, Routine
+
+class Exercise:
+    def __init__(self):
+        pass
+
+    def get_exercise(self, exercise_id):
+        exercise = Exercise.query.filter_by(exercise_id=exercise_id).first()
+        return exercise
+
+    def get_exercises(self):
+        exercises = Exercise.query.all()
+        return exercises
+
+    def add_exercise(self, exercise_name):
+        new_exercise = Exercise(exercise_name)
+        db.session.add(new_exercise)
+        db.session.commit()
+        return new_exercise
+
+    def update_exercise(self, exercise_id, exercise_name):
+        exercise = Exercise.query.filter_by(exercise_id=exercise_id).first()
+        exercise.exercise_name = exercise_name
+        db.session.commit()
+        return exercise
+
+    def delete_exercise(self, exercise_id):
+        exercise = Exercise.query.filter_by(exercise_id=exercise_id).first()
+        db.session.delete(exercise)
+        db.session.commit()
+        return exercise
