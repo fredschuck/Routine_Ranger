e9fef95971c8d378b0a0e888b79c05ff8f561aed
FredSchuck <fredschuck@gmail.com>
2023-10-28 15:28:29 -0400

    Removed get_exercises_by_routine_id

--- src/routine_exercise.py
+++ src/routine_exercise.py
@@ -8,11 +8,11 @@ class RoutinesExercises:
         all_routine_exercises = RoutineExercise.query.all()
         return all_routine_exercises
     
-    def get_exercises_by_routine_id(self, routine_id):
-        '''Returns a list of exercises that are in a routine'''
-        exercise_ids = RoutineExercise.query.filter_by(routine_id=routine_id).with_entities(RoutineExercise.exercise_id).all() #returns tuples
-        exercise_ids_list = [exercise_id[0] for exercise_id in exercise_ids]
-        exercises = Exercise.query.filter(Exercise.exercise_id.in_(exercise_ids_list)).all()
-        return exercises
+    # def get_exercises_by_routine_id(self, routine_id):
+    #     '''Returns a list of exercises that are in a routine'''
+    #     exercise_ids = RoutineExercise.query.filter_by(routine_id=routine_id).with_entities(RoutineExercise.exercise_id).all() #returns tuples
+    #     exercise_ids_list = [exercise_id[0] for exercise_id in exercise_ids]
+    #     exercises = Exercise.query.filter(Exercise.exercise_id.in_(exercise_ids_list)).all()
+    #     return exercises
     
 routine_exercise = RoutinesExercises()
