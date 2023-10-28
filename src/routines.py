66947dd31113c7398bea05e26e76bfb92e63da43
FredSchuck <fredschuck@gmail.com>
2023-10-28 15:28:52 -0400

    Added get_exercises_by_routine_id

--- src/routines.py
+++ src/routines.py
@@ -1,4 +1,4 @@
-from src.models import db, Routine, RoutineExercise
+from src.models import db, Routine, RoutineExercise, Exercise
 
 class Routines:
     '''This class contains methods for interacting with the Routine model'''
@@ -12,6 +12,13 @@ class Routines:
         '''Returns a list of all routine objects'''
         all_routines = Routine.query.all()
         return all_routines
+    
+    def get_exercises_by_routine_id(self, routine_id):
+        '''Returns a list of exercises that are in a routine'''
+        exercise_ids = RoutineExercise.query.filter_by(routine_id=routine_id).with_entities(RoutineExercise.exercise_id).all() #returns tuples
+        exercise_ids_list = [exercise_id[0] for exercise_id in exercise_ids]
+        exercises = Exercise.query.filter(Exercise.exercise_id.in_(exercise_ids_list)).all()
+        return exercises
 
     def add_routine(self, routine_name):
         '''Adds a new routine to the database'''
@@ -27,7 +34,7 @@ class Routines:
         db.session.commit()
         return new_routine_exercise #should it return the exercise instance?
 
-    def update_routine(self, routine_id, routine_name):
+    def edit_routine(self, routine_id, routine_name):
         '''Updates a routine in the database'''
         routine = Routine.query.filter_by(routine_id=routine_id).first()
         routine.routine_name = routine_name
